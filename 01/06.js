e438dcd498a853aaeb9d94c8d2d160255225cdd9
davidcool0117 <davidcool0117@gmail.com>
2022-12-22 11:51:28 +0900

    12/22 연산자와 조건문

--- 01/06.js
+++ 01/06.js
@@ -5,4 +5,16 @@
 let x,y;
 x=7+5;
 y="7"+5;
-document.write(x+y)
+// document.write(x+y)
+document.write(x); // 12
+document.write(y); // 글자7글자5
+document.write("<br>"); // 줄바꿈 태그
+document.write("y"); // 글자y
+// 자바스크립트에서 표현식을 작성할 때 문자는 따옴표로 묶고 변수나 숫자는 묶지 않는다
+// 자바스크립트에서는 태그도 문자열
+// 대상.함수() -> 메서드 -> 임자있는 함수
+const wakeup="선문아 밥먹고 학교 가야지";
+document.write('<h1 style="color:green">' + wakeup + '</h1>');
+
+// es6 에 추가된 템플릿 리터럴
+document.write(`<h1 style="color:green">${wakeup}</h1>`);
---
+++ 02/01.js
@@ -0,0 +1,10 @@
+// 산술연산자 +, -, *, /, %
+const x=75;
+const y=87;
+const add=x+y;
+document.write("국어점수:"+x+"<br>");
+document.write("수학점수:"+y+"<br>");
+document.write("총점:"+add+"<br>");
+document.write("평균:"+add/2+"<hr>");
+// 템플릿리터럴
+document.write(`국어점수:${x}<br>수학점수:${y}<br>총점:${add}<br>평균:${add/2}`);
