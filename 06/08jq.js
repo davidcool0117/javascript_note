52bb2cc125f90a58a8e22b7541147dbea2d25c3a
davidcool0117 <davidcool0117@gmail.com>
2023-01-12 17:36:50 +0900

    1/12 마우스 포인터 변경하기

--- 06/08jq.js
+++ 06/08jq.js
@@ -4,15 +4,27 @@ const progressBar = $(".bar");
 const windowHeight = $(window).height();
 let scrollTop;
 
-$(window).scroll(function () {
-	scrollTop = $(this).scrollTop();
-
+//퍼센트 계산
+function getPercent(sct) {
 	let scrollHeight = section.height();
 	let scrollRealHeight = scrollHeight - windowHeight;
-	let scrollPercent = Math.floor((scrollTop / scrollRealHeight) * 100);
+	//부분/전체*100
+	let scrollPercent = (sct / scrollRealHeight) * 100;
+	let textPercent = Math.floor((sct / scrollRealHeight) * 100);
 	if (scrollPercent >= 100) {
 		scrollPercent = 100;
 	}
-	text.text(scrollPercent + "%");
-	progressBar.css('width' , scrollPercent + "%");
+	render(scrollPercent,textPercent); //argument
+}
+
+//스크롤 양 얻기
+$(window).scroll(function () {
+	scrollTop = $(this).scrollTop();
+	getPercent(scrollTop);
 });
+
+//화면에 출력
+function render(scp,tp) { //parameter, 매개변수
+	text.text(tp + "%");
+	progressBar.css("width", scp + "%");
+}
--- 06/08js.js
+++ 06/08js.js
@@ -1,23 +1,30 @@
 //section 변수에 담기
-const section=document.querySelector('section');
+const section = document.querySelector('section');
 //변수에 담아야할것들
-const text=document.querySelector('.txt');
-const progressBar=document.querySelector('.bar');
+const text = document.querySelector('.txt');
+const progressBar = document.querySelector('.bar');
 //창높이
-const windowHeight=window.innerHeight;
+const windowHeight = window.innerHeight;
 //스크롤양을 담을 변수
 let scrollTop;
-window.addEventListener("scroll",function(){
-    scrollTop=window.pageYOffset || window.scrollY || document.documentElement.scrollTop;
-    /*퍼센트구하기
-        1. 화면높이 windowHeight
-        2. 문서총높이 scrollHeight 변수로 선언 후 ???.clientheight //패딩을 포함한 높이
-        3. scrollRealHeight= 2 - 1
-    */
-   let scrollHeight = section.clientHeight;
-   let scrollRealHeight = scrollHeight-windowHeight;
-   /* 퍼센트로 바꾸기 (부분/전체)*100 */
-   let scrollPercent=(scrollTop/scrollRealHeight)*100
-   text.textContent=Math.floor(scrollPercent)+"%";
-   progressBar.style.width=Math.floor(scrollPercent)+"%";
+
+//스크롤 이벤트 함수
+window.addEventListener("scroll", function () {
+    scrollTop = window.pageYOffset || window.scrollY || document.documentElement.scrollTop;
+    getPercent(scrollTop)
 })
+
+//진행율 계산 함수
+function getPercent(scrollTop){
+    let scrollHeight = section.clientHeight;
+    let scrollRealHeight = scrollHeight - windowHeight;
+    let scrollPercent = (scrollTop / scrollRealHeight) * 100
+    render(scrollPercent)
+}
+
+//화면 출력 함수
+function render(scrollPercent){
+    text.textContent = Math.floor(scrollPercent) + "%";
+    progressBar.style.width = Math.floor(scrollPercent) + "%";
+}
+
