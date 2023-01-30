9a39f1eb543f9c25ad7f5ed72d9181881fd36003
davidcool0117 <davidcool0117@gmail.com>
2023-01-30 09:28:16 +0900

    1/30 tab

---
+++ js_jq/ajax/lightBox/tab_js.js
@@ -0,0 +1,15 @@
+let btn1 = document.querySelectorAll("ul>li");
+let panel1 = document.querySelectorAll("h3");
+
+for(let k = 0; k<panel1.length; k++){
+    panel1[k].style.display ='none';
+}
+
+for(let i = 0; i<btn1.length; i++){
+    btn1[i].addEventListener('click',function(){
+    for(let j = 0; j<panel1.length; j++){
+        panel1[j].style.display ='none';
+    }
+    panel1[i].style.display ='block';
+    })
+}
